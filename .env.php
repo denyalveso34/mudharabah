c07b04fc895f7f7c9fe067c843be1d71e3d6421b
NOFRIAN DENY HENDRAWAN <nofrian.hendrawan@unmer.ac.id>
2023-06-21 21:46:41 +0700

    hu

---
+++ .env.php
@@ -0,0 +1,25 @@
+<?php
+
+/** Application Environment : development testing production */
+define('ENVIRONMENT', 'production');
+
+/** Base Site URL */
+define('env_BASE_URL', 'http://localhost/mudharabah/');
+
+/** MySQL database hostname */
+define('env_HOSTNAME', 'sql.freedb.tech');
+
+/** MySQL database username */
+define('env_USERNAME', 'freedb_kulino');
+
+/** MySQL database password */
+define('env_PASSWORD', 'xs8tG8Yz!@*v4zM');
+
+/** The name of the database */
+define('env_DATABASE', 'freedb_kulino_fprpu');
+
+/** Config Google ClientId */
+define('env_ClientId', '832908224229-33huvklasnal3beel1mgpu2tve47ovhg.apps.googleusercontent.com');
+
+/** Config Google ClientSecret */
+define('env_ClientSecret', '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');
